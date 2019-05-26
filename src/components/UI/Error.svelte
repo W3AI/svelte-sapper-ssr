749a9115e1d406890ad03d965d920c196434ecff
stefian <v_ianta@yahoo.com>
2019-05-26 08:04:06 -0400

    added meetup components

---
+++ src/components/UI/Error.svelte
@@ -0,0 +1,16 @@
+<script>
+    import Modal from './Modal.svelte';
+    import Button from './Button.svelte';
+
+    export let message;
+</script>
+
+<Modal title="An error occured!" on:cancel>
+    <p>{message}</p>
+    <hr>
+    <p>Logging error in issue log...</p>
+    <label>Comment</label>
+    <textarea rows="3">Add issue details here</textarea>
+    <Button>Help!</Button>
+    <p>ToDo: Add Issue Indexing and Ranking engine!</p>
+</Modal>
